eb4c751811c7ee4df02efa827ab62d37f767a231
eziz89 <ezizberdiyev@gmail.com>
2024-06-25 04:48:33 -0700

    4.9.0

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,39 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'title', 'content', 'user_id'
+        'name', 'description'
     ];
 
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
-
-    public function comments() {
-        return $this->hasMany(Comment::class);
-    }
-
-    public function revisions() {
-        return $this->hasMany(Revision::class);
-    }
-
-    public function likes() {
-        return $this->hasMany(Like::class);
-    }
-
-    public function categories() {
-        return $this->belongsToMany(Category::class);
-    }
-
-    public function tags() {
-        return $this->belongsToMany(Tag::class);
-    }
-
-    public function group() {
-        return $this->belongsTo(Group::class);
+    public function articles() {
+        return $this->belongsToMany(Article::class);
     }
 }
